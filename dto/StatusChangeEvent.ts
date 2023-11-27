04e2d28a1f3fb53882b44bc44d7ba83055cc6e92
Antolina <51455932+AntolinaGonzalez@users.noreply.github.com>
2023-11-27 15:19:29 -0300

    Initial commit

---
+++ dto/StatusChangeEvent.ts
@@ -0,0 +1,5 @@
+export interface StatusChangeEvent {
+    order_id: string,
+    status: string,
+    description: string
+}
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import { SQSEvent, SQSRecord } from "aws-lambda";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { StatusChangeEvent } from "../dto/StatusChangeEvent";
+
+const {
+  // variables 
+} = process.env;
+
+const client = new DynamoDB({});
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+const TableName = "order-status";
+
+export const statusLambdaHandler = async (event: SQSEvent) => {
+  try {
+      const statusChangeEvent = event.Records.flatMap(
+        (record: SQSRecord) => JSON.parse(record.body) as StatusChangeEvent
+      )[0];
+      console.info(statusChangeEvent);
+      await ddbDocClient.send(
+          new PutCommand({
+            TableName,
+            Item: {
+              order_id:statusChangeEvent.order_id,
+              order_status: statusChangeEvent.status,
+              description: statusChangeEvent.description
+            },
+            ExpressionAttributeValues: {
+              ':status_reject': 'REJECTED',
+            },
+            ConditionExpression: 'attribute_not_exists(order_id) OR order_status=:status_reject' ,
+          })
+      );
+  } catch (error: any) {
+    console.error(error?.response?.data || error);
+    if (error.name === 'ConditionalCheckFailedException') {
+      return;
+    }
+    throw error;
+  }
+};
